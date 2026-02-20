3baef20723bd48631a133c3493696f8d8d3458d7
webdevSandy <samratgill8204@gmail.com>
2026-02-21 00:14:22 +0530

    feat: fix view btn

--- Backend/utils/emailService.js
+++ Backend/utils/emailService.js
@@ -11,7 +11,7 @@ const sendEmail = async (options) => {
     // ✅ Create transporter using env-based config
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST || "smtp.gmail.com",
-        port: process.env.EMAIL_PORT || 465,
+        port: process.env.EMAIL_PORT || 587 ,
         secure: process.env.EMAIL_PORT == 465 ? true : false,
         auth: {
             user: process.env.EMAIL_USER,
